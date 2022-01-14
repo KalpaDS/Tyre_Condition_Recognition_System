4ce7830b0d914892e4c2d8062e37e62fa592a5ef
KalpaDS <desilvakalpa5@gmail.com>
2022-01-14 23:58:00 +0530

    #finalized

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import io
-import string
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
+from PIL import Image
 from torchvision import models
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
@@ -78,8 +78,18 @@ def register():
 
 @app.route('/pythonlogin/home')
 def home():
+    global prediction_name
     if 'loggedin' in session:
-        return render_template('home.html', username=session['username'])
+        if request.method == 'POST':
+            if 'file' not in request.files:
+                return redirect(request.url)
+            file = request.files.get('file')
+            if not file:
+                return
+            img_bytes = file.read()
+            prediction_name = get_prediction(img_bytes)
+        return render_template('home.html', username=session['username'], name=prediction_name.lower(),
+                               description=conditions[prediction_name])
     return redirect(url_for('login'))
 
 
@@ -90,3 +100,28 @@ model.load_state_dict(torch.load('./fix_resnet18.pth'))
 model.eval()
 
 class_names = ['cracked', 'normal']
+
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, prediction = torch.max(outputs, 1)
+    return class_names[prediction]
+
+
+conditions = {
+    "normal": "You are Safe...",
+    "cracked": "Need Attention...!!!"
+}
